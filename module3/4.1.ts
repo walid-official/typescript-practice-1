fef93dacd16fedea318f906529571e9f1814af67
walid-official <walid.official8@gmail.com>
2025-05-05 19:30:42 +0600

    new function is added

--- module3/4.1.ts
+++ module3/4.1.ts
@@ -1,22 +1,40 @@
 {
   // oop - class
 
-  class Animal {
+// this is a parameter property with public keyword. so that i don't need to write this.name = name and typescript will do it automatically.
+
+  // class Animal {
+  //   constructor(
+  //     public name: string,
+  //     public species: string,
+  //     public sound: string
+  //   ) {}
+
+  //   makeSound() {
+  //     console.log(`The ${this.name} says ${this.sound}`);
+  //   }
+  // }
+
+  // const dog = new Animal("German Shepard Bhai", "dog", "Ghew Ghew");
+  // const cat = new Animal("Persian bhai", "cat", "meaw meaw");
+  // cat.makeSound();
+
+
+  class student {
     constructor(
       public name: string,
-      public species: string,
-      public sound: string
+      public age: number,
+      public hobby: string
     ) {}
 
-    makeSound() {
-      console.log(`The ${this.name} says ${this.sound}`);
+    makeHobby() {
+      console.log(`${this.name} likes ${this.hobby}`);
     }
   }
 
-  const dog = new Animal("German Shepard Bhai", "dog", "Ghew Ghew");
-  const cat = new Animal("Persian bhai", "cat", "meaw meaw");
-
-  cat.makeSound();
+  const student1 = new student("German Shepard Bhai", 20, "playing");
+  const student2 = new student("Persian bhai", 25, "wandering");
+  student1.makeHobby();
 
   //
 }
